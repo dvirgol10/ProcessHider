e3df5199f6adb967146fb8bb7efc7f877496c8a3
Dvir Golan <dvirgol10@gmail.com>
2021-09-08 16:10:36 +0300

    Create program that prints the current processes

---
+++ ProcessList/ProcessList/main.c
@@ -0,0 +1,36 @@
+#include <Windows.h>
+#include <stdio.h>
+#include <winternl.h>
+
+
+typedef NTSTATUS(__stdcall* NtQuerySystemInformationFuncType) (SYSTEM_INFORMATION_CLASS SystemInformationClass, PVOID SystemInformation, ULONG SystemInformationLength, PULONG ReturnLength);
+
+
+int main() {
+
+	//load "ntdll.dll" to retrieve "NtQuerySystemInformation" function.
+	HMODULE hModule = LoadLibraryA("ntdll.dll");
+	NtQuerySystemInformationFuncType NtQuerySystemInformation = (NtQuerySystemInformationFuncType) GetProcAddress(hModule, "NtQuerySystemInformation");
+
+	const bufferSize = 2048 * 2048;
+	byte *processesBuffer = malloc(bufferSize);
+	NTSTATUS status = NtQuerySystemInformation(SystemProcessInformation, (PVOID) processesBuffer, bufferSize, NULL);
+
+	if (!NT_SUCCESS(status)) {
+		printf("[!] Unable to get the system process information: %x\r\n", status);
+		free(processesBuffer);
+		return 1;
+	}
+
+	SYSTEM_PROCESS_INFORMATION* startOfProcessesBuffer = (SYSTEM_PROCESS_INFORMATION*) processesBuffer;
+
+	printf("Process name: %s\r\n", ((SYSTEM_PROCESS_INFORMATION*) processesBuffer)->ImageName.Buffer);
+	while (((SYSTEM_PROCESS_INFORMATION*)processesBuffer)->NextEntryOffset != 0) { //traverse the process list until we reach the end
+		processesBuffer += (((SYSTEM_PROCESS_INFORMATION*) processesBuffer)->NextEntryOffset);
+		printf("Process name: %ws\r\n", ((SYSTEM_PROCESS_INFORMATION*)processesBuffer)->ImageName.Buffer);
+	}
+	
+	free(startOfProcessesBuffer);
+
+	return 0;
+}
